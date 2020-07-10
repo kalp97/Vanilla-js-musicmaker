f2d446b6288b0064d5b1ff59cd7714332ea56ea7
Kalp <kdd32@gmail.com>
2020-07-10 15:20:50 -0400

    Adding js code for all the balls and css for the same

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // JavaScript source code
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#606bd3",
+    "#60c2d3"
+  ];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
